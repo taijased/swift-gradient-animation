e58aa650618fb8812936107b1f9cf09ba03ee46d
Maxim Spiridonov <taijased@gmail.com>
2019-04-23 15:22:36 +0300

    gradient animation

--- GradientBackground/ViewController.swift
+++ GradientBackground/ViewController.swift
@@ -8,13 +8,101 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CAAnimationDelegate {
+    
+    let gradient = CAGradientLayer()
+    
+    var gradientSet = [[CGColor]]()
+    var currentGradient: Int = 0
+    
+    
+    let colorOne = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1).cgColor
+    let colorTwo = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1).cgColor
+    let colorThree = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1).cgColor
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        createGradientView()
+        
+    }
+    
+    /// Creates gradient view
+    
+    func createGradientView() {
+        
+        
+        gradientSet.append([colorOne, colorTwo])
+        gradientSet.append([colorTwo, colorThree])
+        gradientSet.append([colorThree, colorOne])
+        
+        gradient.frame = self.view.bounds
+        gradient.colors = gradientSet[currentGradient]
+        gradient.startPoint = CGPoint(x:1, y:1)
+        gradient.endPoint = CGPoint(x:0, y:1)
+        gradient.drawsAsynchronously = true
+        
+        self.view.layer.insertSublayer(gradient, at: 0)
+        
+        animateGradient()
+    }
+    
+  
+    func animateGradient() {
+        
+        if currentGradient < gradientSet.count - 1 {
+            currentGradient += 1
+        } else {
+            currentGradient = 0
+        }
+        
+        
+        let gradientChangeAnimation = CABasicAnimation(keyPath: "colors")
+        gradientChangeAnimation.duration = 3.0
+        gradientChangeAnimation.toValue = gradientSet[currentGradient]
+        gradientChangeAnimation.fillMode = CAMediaTimingFillMode.forwards
+        gradientChangeAnimation.isRemovedOnCompletion = false
+        gradientChangeAnimation.delegate = self
+        gradient.add(gradientChangeAnimation, forKey: "gradientChangeAnimation")
+    }
+    
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        if flag {
+            gradient.colors = gradientSet[currentGradient]
+            animateGradient()
+        }
+    }
 }
 
+
+
+
+extension UIColor {
+    convenience init? (hexValue: String, alpha: CGFloat) {
+        if hexValue.hasPrefix("#") {
+            let scanner = Scanner(string: hexValue)
+            scanner.scanLocation = 1
+            
+            var hexInt32: UInt32 = 0
+            if hexValue.count == 7 {
+                if scanner.scanHexInt32(&hexInt32) {
+                    let red = CGFloat((hexInt32 & 0xFF0000) >> 16) / 255
+                    let green = CGFloat((hexInt32 & 0x00FF00) >> 8) / 255
+                    let blue = CGFloat(hexInt32 & 0x0000FF) / 255
+                    self.init(red: red, green: green, blue: blue, alpha: alpha)
+                    return
+                }
+            }
+        }
+        return nil
+    }
+}
